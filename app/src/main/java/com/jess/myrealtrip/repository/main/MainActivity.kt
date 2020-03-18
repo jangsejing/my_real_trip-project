8c945248922002ce9e0ab6c7d40b58092de3818a
jangsejin <sejin-011@hanmail.net>
2020-03-18 18:55:20 +0900

    git cache cleared

---
+++ app/src/main/java/com/jess/myrealtrip/repository/main/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.jess.myrealtrip.repository.main
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.jess.myrealtrip.R
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
